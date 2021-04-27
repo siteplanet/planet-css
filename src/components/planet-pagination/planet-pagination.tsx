323f006f130313b06ed042246fe823f787dd0ab4
Sebastian Sørensen <seb@123rail.com>
2021-04-27 19:55:39 +0200

    0.0.117

--- src/components/planet-pagination/planet-pagination.tsx
+++ src/components/planet-pagination/planet-pagination.tsx
@@ -1,4 +1,4 @@
-import { Component, Host, h, Prop } from '@stencil/core';
+import { Component, Event, EventEmitter, Host, h, Prop, State } from '@stencil/core';
 
 @Component({
   tag: 'planet-pagination',
@@ -7,25 +7,77 @@ import { Component, Host, h, Prop } from '@stencil/core';
 })
 export class PlanetPagination {
   /*
+  * List of allowed items per page
+  */
+  @Prop() allowedItemsPerPage: number[];
+  /*
   * Number of current page
   */
-  @Prop() page: number;
+  @Prop({ mutable: true }) page = 1;
   /*
-  * Number of total pages
+  * Number of total items
   */
-  @Prop() pages: number;
+  @Prop() totalItems: number;
+  @State() itemsPerPage = 1;
+  @Event() pUpdate: EventEmitter<{ limit: number, skip: number }>;
+
+  componentWillLoad() {
+    this.itemsPerPage = this.allowedItemsPerPage?.length > 0 ? this.allowedItemsPerPage[0] : 1;
+  }
 
   render() {
+    const { page } = this
+
     return (
       <Host>
         <planet-button-group>
-          <planet-button>&lt;</planet-button>
-          <planet-button>4</planet-button>
-          <planet-button>&gt;</planet-button>
+          <planet-button onClick={() => this.goToPrevious()}>&lt;</planet-button>
+          <planet-button>{page}</planet-button>
+          <planet-button onClick={() => this.goToNext()}>&gt;</planet-button>
+        </planet-button-group>
+
+        <planet-button-group>
+          {this.allowedItemsPerPage.map(item => (
+            <planet-button onClick={() => this.setItemsPerPage(item)} color={item === this.itemsPerPage ? 'success' : 'info'}>{item}</planet-button>
+          ))}
         </planet-button-group>
         <slot></slot>
       </Host>
     );
   }
 
+  public reset() {
+    this.itemsPerPage = this.allowedItemsPerPage?.length > 0 ? this.allowedItemsPerPage[0] : 1;
+    this.page = 1;
+    this.emitUpdate();
+  }
+
+  private setItemsPerPage(max: number) {
+    this.itemsPerPage = max;
+    this.page = 1;
+    this.emitUpdate();
+  }
+
+  private goToPrevious() {
+    if (this.page <= 1) {
+      return;
+    }
+    this.page = this.page - 1;
+    this.emitUpdate();
+  }
+
+  private goToNext() {
+    if (this.page >= Math.ceil(this.totalItems / this.itemsPerPage)) {
+      return;
+    }
+    this.page = this.page + 1;
+    this.emitUpdate();
+  }
+
+  private emitUpdate() {
+    this.pUpdate.emit({
+      limit: this.itemsPerPage,
+      skip: this.itemsPerPage * (this.page - 1),
+    })
+  }
 }
